9f1eaee58419f62c568be06fed86c78d72cf3252
thepieintheface <thepieintheface@users.noreply.github.com>
2020-10-28 16:03:45 -0300

    Improved UI design and general flow

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,39 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(325, 196);
+            this.button1.Location = new System.Drawing.Point(187, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 73);
+            this.button1.Size = new System.Drawing.Size(159, 65);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Download Xml";
+            this.button1.Text = "Create Xml";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select Files to Combine";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(358, 99);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MPC XML Combiner";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,18 +17,169 @@ namespace OrderCombiner
     {
         string outputFile = "";
         string mainFilePath = "";
+        int count = 0;
+        int[] size = { 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 396, 504, 612 };
         public Form1()
-        {
+        {  
+            InitializeComponent();
 
-            
+
+        }
+
+        string EditCount(string toBeEdited)
+        {
+            int editAfter = toBeEdited.IndexOf("<quantity>") + 10;
+            int editBefore = toBeEdited.IndexOf("</quantity>");
+            StringBuilder editedText = new StringBuilder(toBeEdited);
+            editedText.Remove(editAfter, (editBefore - editAfter));
+            editedText.Insert(editAfter, count);
+            return editedText.ToString();
+        }
+        string EditBracket(string toBeEdited)
+        {
+            int editAfter = toBeEdited.IndexOf("<bracket>") + 9;
+            int editBefore = toBeEdited.IndexOf("</bracket>");
+            StringBuilder editedText = new StringBuilder(toBeEdited);
+            editedText.Remove(editAfter, (editBefore - editAfter));
+            int setBracket = 0;
+            for (int i = 0; i < size.Length; i++)
+            {
+                if (count > size[i])
+                {
+                    setBracket = size[i + 1];
+                }
+            }
+            editedText.Insert(editAfter, setBracket);
+            return editedText.ToString();
+        }
+        string ChopAfter(string toBeChopped)
+        {
+            string choppedText = "";
+            int chopAfter = toBeChopped.LastIndexOf("</fronts>");
+            if (chopAfter > 0)
+            {
+                choppedText = toBeChopped.Substring(0, chopAfter);
+            }
+            return choppedText;
+        }
+        string ChopBefore(string toBeChopped)
+        {
+            string choppedText = "";
+            int chopBefore = toBeChopped.IndexOf("<fronts>");
+            if (chopBefore > 0)
+            {
+                choppedText = toBeChopped.Substring(toBeChopped.IndexOf("<fronts>") + 8);
+            }
+            return choppedText;
+        }
+        char getFirstDigit(int num)
+        {
+            char[] numHolder = num.ToString().ToCharArray();
+            return numHolder[0];
+        }
+        char getSecondDigit(int num)
+        {
+            char[] numHolder = num.ToString().ToCharArray();
+            return numHolder[1];
+        }
+        char getThirdDigit(int num)
+        {
+            char[] numHolder = num.ToString().ToCharArray();
+            return numHolder[2];
+        }
+        string ParseString(string inputString)
+        {
+            StringBuilder buildingString = new StringBuilder(inputString);
+            List<int> deletions = new List<int>();
+
+
+
+            for (int i = 1; i < buildingString.Length - 2; i++)
+            {
+
+                if (Char.IsDigit(buildingString[i]) && (buildingString[i - 1] == '[' || buildingString[i - 1] == ','))
+                {
+                    //only working way i found to convert int to char
+
+                    buildingString[i] = getFirstDigit(count);
+
+                    if (Char.IsDigit(buildingString[i + 1]) && count > 9)
+                    {
+
+                        // checks second number
+
+                        buildingString[i + 1] = getSecondDigit(count);
+
+                        if (Char.IsDigit(buildingString[i + 2]))
+                        {
+                            //checks 3rd number
+
+
+                            buildingString[i + 2] = getThirdDigit(count);
+
+
+
+
+
+                            i++;
+                        }
+                        else if (Char.IsDigit(buildingString[i + 2]) && count <= 99)
+                        {
+
+                            deletions.Add(i + 2);
+
+
+                        }
+                        else if (!Char.IsDigit(buildingString[i + 2]) && count > 99)
+                        {
+                            buildingString.Insert(i + 2, count.ToString()[2]);
+                            i++;
+
+                        }
+
+                        i++;
+                    }
+                    else if (Char.IsDigit(buildingString[i + 1]) && count <= 9)
+                    {
+
+                        deletions.Add(i + 1);
+
+
+                    }
+                    else if (!Char.IsDigit(buildingString[i + 1]) && count > 9)
+                    {
+                        buildingString.Insert(i + 1, count.ToString()[1]);
+                        i++;
+
+                    }
+                    Console.WriteLine("cycle: " + i + "count: " + count);
+
+
+
+                    count++;
+                }
+
+            }
+
+
+
+            for (int i = deletions.Count - 1; i >= 0; i--)
+            {
+
+                buildingString.Remove(deletions[i], 1);
+            }
+            return buildingString.ToString();
+        }
+        void SelectFiles()
+        {
             var firstFileContent = string.Empty;
-            int count = 0;
-            int[] size = { 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, 198, 216, 234, 396, 504, 612 };
-            
+
+
+
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "c:\\";
-              
+
                 openFileDialog.RestoreDirectory = true;
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
@@ -54,8 +205,8 @@ namespace OrderCombiner
 
                 if (openFileDialog2.ShowDialog() == DialogResult.OK)
                 {
-                    
-                    
+
+
                     var fileStream = openFileDialog2.OpenFile();
 
                     using (StreamReader reader = new StreamReader(fileStream))
@@ -71,161 +222,8 @@ namespace OrderCombiner
             outputFile = ParseString(outputFile);
             outputFile = EditCount(outputFile);
             outputFile = EditBracket(outputFile);
-            InitializeComponent();
-
-
-
-
-
-            string EditCount(string toBeEdited)
-            {
-                int editAfter = toBeEdited.IndexOf("<quantity>")+10;
-                int editBefore = toBeEdited.IndexOf("</quantity>");
-                StringBuilder editedText = new StringBuilder(toBeEdited);
-                editedText.Remove(editAfter,(editBefore-editAfter));
-                editedText.Insert(editAfter, count);
-                return editedText.ToString();
-            }
-            string EditBracket(string toBeEdited)
-            {
-                int editAfter = toBeEdited.IndexOf("<bracket>") + 9;
-                int editBefore = toBeEdited.IndexOf("</bracket>");
-                StringBuilder editedText = new StringBuilder(toBeEdited);
-                editedText.Remove(editAfter, (editBefore - editAfter));
-                int setBracket = 0;
-                for(int i=0; i<size.Length; i++)
-                {
-                    if (count > size[i])
-                    {
-                        setBracket = size[i+1];
-                    }
-                }
-                editedText.Insert(editAfter, setBracket);
-                return editedText.ToString();
-            }
-            string ChopAfter(string toBeChopped)
-            {
-                string choppedText = "";
-                int chopAfter = toBeChopped.LastIndexOf("</fronts>");
-                if (chopAfter > 0)
-                {
-                    choppedText = toBeChopped.Substring(0, chopAfter);
-                }
-                return choppedText;
-            }
-            string ChopBefore(string toBeChopped)
-            {
-                string choppedText = "";
-                int chopBefore = toBeChopped.IndexOf("<fronts>");
-                if (chopBefore > 0)
-                {
-                    choppedText = toBeChopped.Substring(toBeChopped.IndexOf("<fronts>")+8);
-                }
-                return choppedText;
-            }
-            char getFirstDigit(int num)
-            {
-                char[] numHolder = num.ToString().ToCharArray();
-                return numHolder[0];
-            }
-            char getSecondDigit(int num)
-            {
-                char[] numHolder = num.ToString().ToCharArray();
-                return numHolder[1];
-            }
-            char getThirdDigit(int num)
-            {
-                char[] numHolder = num.ToString().ToCharArray();
-                return numHolder[2];
-            }
-            string ParseString(string inputString)
-            {
-                StringBuilder buildingString = new StringBuilder(inputString);
-                List<int> deletions = new List<int>();
-                
-
-
-                for (int i = 1; i < buildingString.Length - 2; i++)
-                {
-
-                    if (Char.IsDigit(buildingString[i]) && (buildingString[i - 1] == '[' || buildingString[i - 1] == ','))
-                    {
-                        //only working way i found to convert int to char
-
-                        buildingString[i] = getFirstDigit(count);
-
-                        if (Char.IsDigit(buildingString[i + 1]) && count > 9)
-                        {
-
-                            // checks second number
-
-                            buildingString[i + 1] = getSecondDigit(count);
-
-                            if (Char.IsDigit(buildingString[i + 2]))
-                            {
-                                //checks 3rd number
-
-
-                                buildingString[i + 2] = getThirdDigit(count);
-
-
-
-
-
-                                i++;
-                            }
-                            else if (Char.IsDigit(buildingString[i + 2]) && count <= 99)
-                            {
-
-                                deletions.Add(i + 2);
-
-
-                            }
-                            else if (!Char.IsDigit(buildingString[i + 2]) && count > 99)
-                            {
-                                buildingString.Insert(i + 2, count.ToString()[2]);
-                                i++;
-
-                            }
-
-                            i++;
-                        }
-                        else if (Char.IsDigit(buildingString[i + 1]) && count <= 9)
-                        {
-
-                            deletions.Add(i + 1);
-
-
-                        }
-                        else if (!Char.IsDigit(buildingString[i + 1]) && count > 9)
-                        {
-                            buildingString.Insert(i + 1, count.ToString()[1]);
-                            i++;
-
-                        }
-                        Console.WriteLine("cycle: " + i + "count: " + count);
-
-
-
-                        count++;
-                    }
-
-                }
-
-
-
-                for (int i = deletions.Count - 1; i >= 0; i--)
-                {
-
-                    buildingString.Remove(deletions[i], 1);
-                }
-                return buildingString.ToString();
-            }
-
-
         }
 
-        
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -239,5 +237,11 @@ namespace OrderCombiner
             }
             
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SelectFiles();
+        }
+
     }
 }
